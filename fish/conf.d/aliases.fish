e5c04031247481c50cb991676cbbf16c11149b54
Antonio Garcia <35939044+lantoniogc@users.noreply.github.com>
2026-08-29 23:00:33 +0200

    fish: alias vim~>helix. Include mise, fnox.

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,6 +1,5 @@
-alias vim 'nvim'
-alias top 'btop'
-alias ls 'lsd'
-alias pbc 'pbcopy'
-alias pbp 'pbpaste'
-
+alias vim hx
+alias top btop
+alias ls lsd
+alias pbc pbcopy
+alias pbp pbpaste
---
+++ fish/conf.d/mise.fish
@@ -0,0 +1,6 @@
+mise activate fish | source
+
+status is-interactive
+or set -gx FNOX_SHELL_OUTPUT none
+
+fnox activate fish | source
