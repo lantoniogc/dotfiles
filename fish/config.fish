56a393c83059a77f03a54679686eda8ad199eb94
Antonio Garcia <35939044+lantoniogc@users.noreply.github.com>
2026-09-25 16:48:00 +0200

    fish: set explicit color theme mode (dark).

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,3 @@
-set fish_greeting 
-set -gx EDITOR 'nvim'
-fish_config theme choose "catppuccin-mocha"
-
+set fish_greeting
+set -gx EDITOR nvim
+fish_config theme choose --color-theme=dark catppuccin-mocha
